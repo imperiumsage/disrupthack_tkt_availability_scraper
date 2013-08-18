856a6f9096b540f592c145347041bc4f83e99f2b
Sai Shenoy <sshenoy@kabam.com>
2013-08-17 23:44:05 -0700

    Added tweet capability

--- tcdisrupt.php
+++ tcdisrupt.php
@@ -2,8 +2,29 @@
 date_default_timezone_set('America/Los_Angeles');
 require_once("lib/simple_html_dom.php");
 require_once("lib/twilio-php-master/Services/Twilio.php");
+require_once("lib/TwitterAPIExchange.php");
 $config = parse_ini_file("tcdisrupt.ini");
 $hour = date('G');
+
+function sendTweet($message) {
+	global $config;
+	$settings = array(
+	    'oauth_access_token' => $config['twitter_oauth_token'],
+	    'oauth_access_token_secret' => $config['twitter_oauth_secret'],
+	    'consumer_key' => $config['twitter_consumer_key'],
+	    'consumer_secret' => $config['twitter_consumer_secret']
+	);
+	$url = "https://api.twitter.com/1.1/statuses/update.json";
+	$requestMethod = 'POST';
+	$postfields = array(
+    	'status' => $message
+	);
+	$twitter = new TwitterAPIExchange($settings);
+	echo $twitter->buildOauth($url, $requestMethod)
+	             ->setPostfields($postfields)
+	             ->performRequest();
+}
+
 function sendMsg($message,$number) {
 	global $config;
 	// set your AccountSid and AuthToken from www.twilio.com/user/account
@@ -31,8 +52,10 @@ $rowCells = $html->find('tr[class="ticket_row"] td');
 $cellContents = $rowCells[4]->innertext;
 if(stristr($cellContents,"'soldout'") !== false || stristr($cellContents,"'na'") !== false) {
 	print date('Y-m-d H:i:s').":Still soldout!\n";
+	//sendTweet("Still sold out!");
 } else {
 	sendMsg("DisruptHack tickets available at $URL",$config['to_phone']);
+	sendTweet("DisruptHack tickets available at $URL");
 }
 
 
